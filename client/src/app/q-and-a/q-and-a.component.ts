b49d5b906579fb16e4faa49cb8bd5499a5d1c73e
VladislavMakshaev <vlad.makshaev@gmail.com>
2020-03-25 19:11:02 +0400

    Front+Back merged - front does to https://quiz-back2020.herokuapp.com/ instead of localhost

--- client/src/app/q-and-a/q-and-a.component.ts
+++ client/src/app/q-and-a/q-and-a.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {QuestionsId} from "../quiz-setup/QuestionsId";
+import {QuestionsId} from "../entity/QuestionsId";
 import {questionStorageService} from "../_services/questionStorage.Service";
-import {HttpClientService} from "../quiz-setup/http-client.service";
+import {HttpClientService} from "../_services/http-client.service";
+import {Question} from "../entity/Question";
+import {TokenStorageService} from "../_services/token-storage.service";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -10,29 +13,58 @@ import {HttpClientService} from "../quiz-setup/http-client.service";
   styleUrls: ['./q-and-a.component.css']
 })
 export class QandAComponent implements OnInit {
-  questionIdList: QuestionsId[];
-  selectedTheme;
-  constructor(private questionStorageService: questionStorageService) {
+  private questionIdList: QuestionsId[];
+  private qUrl: string = 'https://quiz-back2020.herokuapp.com/questions/';
+  private ansUrl: string = 'https://quiz-back2020.herokuapp.com/results';
+  private nextQestionId: QuestionsId;
+  private question: Question;
+  private user;
+  private answerId;
+
+  constructor(
+    private qStorageService: questionStorageService,
+    private httpClient: HttpClientService,
+    private tokenStorageService: TokenStorageService,
+    private router: Router
+  ) {
+
   }
 
   ngOnInit(): void {
-    this.questionIdList = this.questionStorageService.getQuestionList();
-    this.selectedTheme = this.questionStorageService.getSelectedTheme();
-    console.log(this.selectedTheme);
-    console.log(this.questionStorageService.getSelectedTheme());
-    debugger;
+    this.questionIdList = this.qStorageService.getQuestionIdList();
+    this.firstQuestion();
+    this.user = this.tokenStorageService.getUser();
+  }
+
+  firstQuestion() {
+    if (this.questionIdList.length > 0) {
+      this.nextQestionId = this.questionIdList.shift();
+      this.httpClient.getQuestion(this.nextQestionId, this.qUrl)
+        .subscribe(question => {
+          this.question = question;
+        });
+    }
   }
 
 
-  nextQuestion() {
-    if (this.questionIdList.length > 0) {
-      this.questionStorageService.getQuestionList().splice(0, 1);
-      this.questionIdList.splice(0,1);
+  nextQuestion(event) {
+    // for (let i = 0; i <= this.question.answers.length; i++) {
+    //   if (this.question.answers.includes(event)) {
+    //     this.answerId = this.question.answers[i].id;
+    //   }
+    // }
+    debugger;
+    this.httpClient.postAnswer(this.user.id, event, this.ansUrl);
 
+    if (this.questionIdList.length > 0) {
+      this.nextQestionId = this.questionIdList.shift();
+      this.httpClient.getQuestion(this.nextQestionId, this.qUrl)
+        .subscribe(question => {
+          this.question = question;
+        });
     } else {
-      //this.questionStorageService.clearData();
+      this.router.navigateByUrl('user/result');
     }
-    console.log(this.questionStorageService.getQuestionList());
   }
 
 
--- client/src/app/quiz-setup/quiz-setup.component.ts
+++ client/src/app/quiz-setup/quiz-setup.component.ts
@@ -1,11 +1,10 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import {HttpClientService} from "./http-client.service";
-import {Dif} from "./Dif";
-import {Theme} from "./Theme";
-import {QuestionsId} from "./QuestionsId";
 import {questionStorageService} from "../_services/questionStorage.Service";
-import { ActivatedRoute } from '@angular/router';
+import {Theme} from "../entity/Theme";
+import {Dif} from "../entity/Dif";
+import {HttpClientService} from "../_services/http-client.service";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -13,83 +12,67 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './quiz-setup.component.html',
   styleUrls: ['./quiz-setup.component.css']
 })
-export class QuizSetupComponent implements OnInit, OnDestroy {
-  difs: Dif[];
-  selectedDif;
-  serviceSelectedDif;   //for debugging
-  difUrl = "http://localhost:8080/difficult";
+export class QuizSetupComponent implements OnInit {
 
-  themes: Theme[];
-  selectedTheme;
-  serviceSelctedTheme;    //for debugging
-  themeUrl = "http://localhost:8080/theme";
+  themes ?: Theme[];
+  difs ?: Dif[];
 
-  questionIdList: QuestionsId[];
-  takeQuestionUrl = 'http://localhost:8080/questions/ThemeAndDifId';
+  difUrl = "https://quiz-back2020.herokuapp.com/difficult";
+  themeUrl = "https://quiz-back2020.herokuapp.com/theme";
+  takeQuestionUrl = 'https://quiz-back2020.herokuapp.com/questions/ThemeAndDifId';
 
   constructor(
-    private httpClientService: HttpClientService,
-    private questionStorageService: questionStorageService,
-    private route: ActivatedRoute
+    private httpClient: HttpClientService,
+    private qStorageService: questionStorageService,
+    private router: Router
   ) {
   }
 
   ngOnInit(): void {
-    this.getDifs();
-    this.getThemes();
+    this.initThemesDifs();
   }
 
-  ngOnDestroy(): void {
-    this.questionStorageService.setQuestionList(this.questionIdList);
+  initThemesDifs() {
+    this.httpClient.getDifs(this.difUrl)
+      .subscribe(diffs => {
+        this.qStorageService.setDifs(diffs);
+        this.difs = diffs;
+      });
+    this.httpClient.getThemes(this.themeUrl)
+      .subscribe(themes => {
+        this.qStorageService.setThemes(themes);
+        this.themes = themes;
+      });
   }
 
-  getDifs(): void {
-    this.httpClientService.getDifs(this.difUrl)
-      .subscribe(diffs => this.difs = diffs);
-  }
 
-  getThemes(): void {
-    this.httpClientService.getThemes(this.themeUrl)
-      .subscribe(themes => this.themes = themes);
-  }
-
-  getQuestionList() {
-    this.httpClientService.getQuestionList(
-      this.questionStorageService.getSelectedTheme(),
-      this.questionStorageService.getSelctedDif(),
-      '5',
-      this.takeQuestionUrl)
-      .subscribe(qId => this.questionStorageService.setQuestionList(qId));
-    this.questionIdList = this.questionStorageService.getQuestionList();
-
-  }
-
-  setSelectedTheme(event) {
-    let x = event.target.value;
-    console.log(this.questionStorageService.getQuestionList());
-    //console.log(x);
-   for (let i = 0; i <= this.themes.length; i++){
-     console.log(this.themes[i].themeName);
-      if (this.themes[i].themeName.includes(x)){
-        console.log("change theme");
-        this.questionStorageService.setTheme(this.themes[i].id);
-        break;
-      }
-
-    }
-  }
-
-  setSelectedDif(event) {
+  changeStockDif(event) {
     let x = event.target.value;
     for (let i = 0; i <= this.difs.length; i++) {
       if (this.difs[i].difficultName.includes(x)) {
-        this.questionStorageService.setDif(this.difs[i].id);
-        break;
+        this.qStorageService.setSelectedDif(this.difs[i].id);
       }
     }
   }
 
+  changeStockTheme(event) {
+    let x = event.target.value;
+    for (let i = 0; i <= this.themes.length; i++) {
+      if (this.themes[i].themeName.includes(x)) {
+        this.qStorageService.setSelectedTheme(this.themes[i].id);
+      }
+    }
+  }
 
-
-
+  getQuestionList() {
+    this.httpClient.getQuestionList(
+      this.qStorageService.getSelectedTheme(),
+      this.qStorageService.getSelectedDif(),
+      '5',
+      this.takeQuestionUrl)
+      .subscribe(qId => {
+        this.qStorageService.setQuestionIdList(qId);
+        this.router.navigateByUrl('user/qanda');
+      });
+  }
 }
