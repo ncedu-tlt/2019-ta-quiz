592ef20a64e8ea981cbdf634912ff0942b14cb0a
ibnvladimir <iidesign@yandex.ru>
2020-04-12 08:59:04 +0400

    fix swapped theme and history

--- client/src/app/_services/mock-user-history.service.ts
+++ client/src/app/_services/mock-user-history.service.ts
@@ -10,15 +10,15 @@ export class MockUserHistoryService {
 		{
 			"idGame": "f87681f2-0224-4dba-a58b-7fdb4b1ef337" ,
 			"date": "2020-04-02 14:36:43" ,
-			"theme": "Легкий" ,
-			"difficult": "История" ,
+			"theme": "История" ,
+			"difficult": "Легкий" ,
 			"score": 200
 		},
 		{
 			"idGame": "b9d65335-9277-4038-892d-477cb94c755f" ,
 			"date": "2020-04-06 20:29:25" ,
-			"theme": "Легкий" ,
-			"difficult": "История" ,
+			"theme": "История" ,
+			"difficult": "Легкий" ,
 			"score": 300
 		}
 		]
