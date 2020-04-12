ed4191f64061ea5cd4349473ce67b1b3c120a20c
ibnvladimir <iidesign@yandex.ru>
2020-04-12 08:44:35 +0400

    add getHistoryMethod

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TokenStorageService } from '../_services/token-storage.service';
+import { MockUserHistoryService } from '../_services/mock-user-history.service';
+import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +11,26 @@ import { TokenStorageService } from '../_services/token-storage.service';
 })
 export class ProfileComponent implements OnInit {
   currentUser: any;
+  private history  = this.mockHistory.body;
+  private URLForHistory;
 
-  constructor(private token: TokenStorageService) { }
+  constructor(
+    private http: HttpClient,
+    private token: TokenStorageService,
+    private mockHistory: MockUserHistoryService,
+    ) { }
 
   ngOnInit() {
     this.currentUser = this.token.getUser();
   }
+
+  getHistory(){
+    this.http.get<any>(this.URLForHistory,{})
+    .subscribe(obj =>{
+        this.history = obj;
+    })
+}
+
+
+
 }
