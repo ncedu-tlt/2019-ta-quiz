910abc9b0556ecc8b09e9e3e579c069fa1218668
ibnvladimir <iidesign@yandex.ru>
2020-04-12 10:10:32 +0400

    template for user history with specialty

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../_services/token-storage.service';
 import { MockUserHistoryService } from '../_services/mock-user-history.service';
 import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import { MockResultService } from '../_services/mock-result.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,16 +14,23 @@ export class ProfileComponent implements OnInit {
   currentUser: any;
   private history;
   private URLForHistory;
+  private isShowDetails: boolean = false;
+  private URLToResult;
+  private results;
+  private fullQuestion;
 
   constructor(
     private http: HttpClient,
     private token: TokenStorageService,
     private mockHistory: MockUserHistoryService,
+    private mockResult: MockResultService,
     ) { }
 
   ngOnInit() {
     this.currentUser = this.token.getUser();
     this.history = this.mockHistory.body;
+    this.results = this.mockResult.body;
+    this.fullQuestion = this.results.questions;
   }
 
   getHistory(){
@@ -30,8 +38,28 @@ export class ProfileComponent implements OnInit {
     .subscribe(obj =>{
         this.history = obj;
     })
+  }
+
+  getResult(){
+    this.http.get<any>(this.URLToResult,{})
+    .subscribe(obj =>{
+        this.results = obj;
+    })
+  }
+
+  showOutputDetails(val) {
+    if(val.srcElement.nextElementSibling.style['display']=="block") {
+      val.srcElement.nextElementSibling.style['display']="none";
+
+    } else {
+      val.srcElement.nextElementSibling.style['display']="block";
+    }
+
+
+
+  }
 }
 
 
 
-}
+
