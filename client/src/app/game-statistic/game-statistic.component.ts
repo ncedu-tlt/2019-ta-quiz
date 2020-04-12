5b23deea0fc7c4cf1bd3e93ed873ebacd4b822a5
ibnvladimir <iidesign@yandex.ru>
2020-04-13 00:08:15 +0400

    init gameStatistic component

---
+++ client/src/app/game-statistic/game-statistic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-statistic',
+  templateUrl: './game-statistic.component.html',
+  styleUrls: ['./game-statistic.component.css']
+})
+export class GameStatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
